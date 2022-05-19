34ea5adb27e90b2dfe81a13fa038d613368aa9bf
artem1101gavrilov <artem1101gavrilov@gmail.com>
2022-05-19 23:43:28 +0300

    Пример работы с конфигурационным файлом.

---
+++ UpdateConfiguration/UpdateConfiguration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+
+namespace UpdateConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"{TestConfiguration.Instance.TestSection.Value} {(ConfigurationManager.GetSection(nameof(TestConfiguration)) as TestConfiguration).TestSection.Value}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ UpdateConfiguration/UpdateConfiguration/TestConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+
+namespace UpdateConfiguration
+{
+    public class TestConfiguration : ConfigurationSection
+    {
+        public static TestConfiguration Instance { get; private set; }
+
+        static TestConfiguration()
+        {
+            try
+            {
+                Instance = (TestConfiguration)ConfigurationManager.GetSection(nameof(TestConfiguration));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private TestConfiguration() { }
+
+        [ConfigurationProperty(nameof(TestSection), IsRequired = false)]
+        public TestSection TestSection
+        {
+            get => (TestSection)this[nameof(TestSection)];
+            set => this[nameof(TestSection)] = value;
+        }
+    }
+
+    public class TestSection : ConfigurationElement
+    {
+        private const string SectionValue = "value";
+        [ConfigurationProperty(SectionValue, IsRequired = false)]
+        public int Value
+        {
+            get => (int)this[SectionValue];
+            set => this[SectionValue] = value;
+        }
+    }
+}
